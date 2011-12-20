cbe57d43fa61e3300c4115fb7e7f1413d1485503
inugent <>
2011-12-20 10:08:44 +0000

    patched histoConfig bug

--- Code/HistoConfig.cxx
+++ Code/HistoConfig.cxx
@@ -94,8 +94,7 @@ HistoConfig::~HistoConfig(){
 //utility Functions
 bool HistoConfig::GetHisto(bool isdata,int id,unsigned int &histo){
   if(isdata){
-    histo=1;
-    return true;
+    id==1;
   }
   for(int i=0; i<ID.size(); i++){
     if(ID.at(i)==id){
