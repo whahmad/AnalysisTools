70f3a1700646bf4211ee17c43387d77d8a256689
inugent <>
2012-01-22 19:47:24 +0000

    adding TauSpinerInterface.h

---
+++ Code/TauSpinerInterface.h
@@ -0,0 +1,17 @@
+#ifndef TauSpinerInterface_h
+#define TauSpinerInterface_h
+
+#include "SimpleParticle.h"
+#include <vector>
+
+class TauSpinerInterface {
+
+ public:
+  TauSpinerInterface();
+  ~TauSpinerInterface();
+
+  enum TauSpinerType {Spin=0,UnSpin,FlipSpin};
+  double Weight(TauSpinerType type, SimpleParticle X, SimpleParticle tau, std::vector<SimpleParticle> tau_daughters,SimpleParticle tau2, std::vector<SimpleParticle> tau_daughters2);
+
+};
+#endif
